bdcb08931c1d9626d0a6f64181ac078ffb6aaf7d
164367 MUHAMAD <164367.MUHAMAD@klgroup.local>
2023-02-08 22:16:53 +0700

    230230208

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         // custom middleware
-        'cekrole' => \App\Http\Middleware\CekRole::class,
+        'CheckPermission' => \App\Http\Middleware\CheckPermission::class,
     ];
 }
