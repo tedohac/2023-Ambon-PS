44b956e123289ffcfe9309570a54bac580970226
164367 MUHAMAD <164367.MUHAMAD@klgroup.local>
2023-02-09 21:30:51 +0700

    20230209

--- public/dist/js/newkip.js
+++ public/dist/js/newkip.js
@@ -28,17 +28,14 @@
         {
             if (this.files && this.files[0]) {
                 var reader = new FileReader();
-                console.log("read file");
                 
-                // $("#profilethumb1").empty();
+                $("#profilethumb1").empty();
                 reader.onload = function(e) {
                     $("#profilethumb1").append('<img id="previewpict1" style="max-width: 200px;" class="bg-white border p-1"');
                     $('#previewpict1').attr('src', e.target.result);
                 }
-                console.log("load preview");
                 
                 reader.readAsDataURL(this.files[0]); // convert to base64 string
-                console.log("finish");
             }
         });
         
--- resources/views/kip/new.blade.php
+++ resources/views/kip/new.blade.php
@@ -182,4 +182,65 @@
 
 <!-- JS -->
 <script src="{{ asset('dist/js/newkip.js') }}"></script>
+
+<script>
+  
+  $(document).ready(function(){
+  
+        
+  // Select2
+  $('.select2bs4').select2({
+      placeholder: "Select",
+      id: '-1', // the value of the option
+      theme: 'bootstrap4'
+  });
+  
+  // Jodit
+  $('.jodit').each(function () {
+      var editor = new Jodit(this, {
+          "spellcheck": false,
+          "buttons": "undo,redo,|,bold,underline,italic,|,superscript,subscript,|,ul,ol,|,outdent,indent,align,fontsize,|,image,link,|",
+      });
+  });
+
+  // Class Image Uploader
+  $(".custom-file-input").on("change", function() {
+      var fileName = $(this).val().split("\\").pop();
+      $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+  });
+
+  // Image Preview 1
+  $("#kip_foto_sebelum").change(function()
+  {
+      if (this.files && this.files[0]) {
+          var reader = new FileReader();
+          
+          $("#profilethumb1").empty();
+          reader.onload = function(e) {
+              $("#profilethumb1").append('<img id="previewpict1" style="max-width: 200px;" class="bg-white border p-1"');
+              $('#previewpict1').attr('src', e.target.result);
+          }
+          
+          reader.readAsDataURL(this.files[0]); // convert to base64 string
+      }
+  });
+  
+  // Image Preview 1
+  $("#kip_foto_sesudah").change(function()
+  {
+      if (this.files && this.files[0]) {
+          var reader = new FileReader();
+          
+          $("#profilethumb2").empty();
+          reader.onload = function(e) {
+              $("#profilethumb2").append('<img id="previewpict2" style="max-width: 200px;" class="bg-white border p-1"');
+              $('#previewpict2').attr('src', e.target.result);
+          }
+          
+          reader.readAsDataURL(this.files[0]); // convert to base64 string
+      }
+  });
+
+});
+</script>
 @endsection
