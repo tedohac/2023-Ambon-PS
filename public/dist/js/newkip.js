9232c1c58775c67b961eea667317dc00e6af1bb2
164367 MUHAMAD <164367.MUHAMAD@klgroup.local>
2023-02-09 23:01:32 +0700

    20230209

--- public/dist/js/newkip.js
+++ public/dist/js/newkip.js
@@ -54,6 +54,7 @@
         // on form submit
         $('button[type="submit"]').on('click',function(e)
         {
+            console.log('asd');
             e.preventDefault();
             var submit_value = $(this).val();
             jQuery.post
