85764fc5249bbac74c630d7298e1e673c1db3554
164367 MUHAMAD <164367.MUHAMAD@klgroup.local>
2023-02-09 23:11:38 +0700

    20230209

--- app/Http/Controllers/KipController.php
+++ app/Http/Controllers/KipController.php
@@ -21,7 +21,6 @@ class KipController extends Controller
     
     public function save(Request $request)
     {    	
-        echo "asdasd";
         $lastKip = Kip::orderBy('kip_no', 'DESC')->first();
 
         if(!empty($lastKip))
@@ -30,6 +29,26 @@ class KipController extends Controller
             $kip_no = $kip_no + 1;
             $kip_no = "SS".str_pad($value, 5, '0', STR_PAD_LEFT);
         }
-        echo $lastKip;
+        else
+        {
+            $kip_no = "SS00001";
+        }
+        
+        if ($request->hasFile('kip_foto_sebelum')) 
+        {
+            try
+            {
+                $kip_foto_sebelum = $kip_no.'.'.$request->file('kip_foto_sebelum')->getClientOriginalExtension();
+                    
+                // delete if exists
+                if (Storage::disk('public')->exists( 'kip/'.$kip_foto_sebelum )) Storage::delete('public/kip/'.$kip_foto_sebelum);
+                $request->file('kip_foto_sebelum')->storeAs('public/kip', $kip_foto_sebelum);
+
+                Artisan::call('cache:clear');
+            } catch (\Illuminate\Database\QueryException $e) {
+                Session::flash('error', 'Upload kip_foto_sebelum failed');
+                return redirect()->back();
+            }
+        }
     }
 }
