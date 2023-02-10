4a4cbd64991eac14072834cbaff3687efa10b21f
164367 MUHAMAD <164367.MUHAMAD@klgroup.local>
2023-02-10 17:02:39 +0700

    20230210

--- database/migrations/2023_02_10_142440_create_nilais_table.php
+++ database/migrations/2023_02_10_142440_create_nilais_table.php
@@ -16,15 +16,15 @@ class CreateNilaisTable extends Migration
         Schema::create('kips', function (Blueprint $table) {
             $table->string('nilai_kip_no', 20);
             $table->string('nilai_created_by', 10);
-            $table->int('nilai_penghematan');
-            $table->int('nilai_quality');
-            $table->int('nilai_safety');
-            $table->int('nilai_ergonomi');
-            $table->int('nilai_manfaat');
-            $table->int('nilai_kepekaan');
-            $table->int('nilai_keaslian');
-            $table->int('nilai_usaha');
-            $table->int('nilai_total');
+            $table->integer('nilai_penghematan');
+            $table->integer('nilai_quality');
+            $table->integer('nilai_safety');
+            $table->integer('nilai_ergonomi');
+            $table->integer('nilai_manfaat');
+            $table->integer('nilai_kepekaan');
+            $table->integer('nilai_keaslian');
+            $table->integer('nilai_usaha');
+            $table->integer('nilai_total');
             $table->primary(array('nilai_kip_no', 'nilai_created_by'));
             $table->foreign('nilai_kip_no')->references('kip_no')->on('kips');
             $table->foreign('nilai_created_by')->references('user_npk')->on('users');
