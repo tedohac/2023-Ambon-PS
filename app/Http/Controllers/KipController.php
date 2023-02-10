3e4bfcff6de257799a45431b50c11f4d5c9a11a1
164367 MUHAMAD <164367.MUHAMAD@klgroup.local>
2023-02-10 08:55:23 +0700

    20230210

--- app/Http/Controllers/KipController.php
+++ app/Http/Controllers/KipController.php
@@ -36,73 +36,75 @@ class KipController extends Controller
         {
             $kip_no = "SS00001";
         }
+
+        echo $kip_no;
         
-        if ($request->hasFile('kip_foto_sebelum')) 
-        {
-            try
-            {
-                $kip_foto_sebelum = $kip_no.'-sebelum.'.$request->file('kip_foto_sebelum')->getClientOriginalExtension();
+        // if ($request->hasFile('kip_foto_sebelum')) 
+        // {
+        //     try
+        //     {
+        //         $kip_foto_sebelum = $kip_no.'-sebelum.'.$request->file('kip_foto_sebelum')->getClientOriginalExtension();
                     
-                // delete if exists
-                if (Storage::disk('public')->exists( 'kip/'.$kip_foto_sebelum )) Storage::delete('public/kip/'.$kip_foto_sebelum);
-                $request->file('kip_foto_sebelum')->storeAs('public/kip', $kip_foto_sebelum);
+        //         // delete if exists
+        //         if (Storage::disk('public')->exists( 'kip/'.$kip_foto_sebelum )) Storage::delete('public/kip/'.$kip_foto_sebelum);
+        //         $request->file('kip_foto_sebelum')->storeAs('public/kip', $kip_foto_sebelum);
 
-                Artisan::call('cache:clear');
-            } catch (\Illuminate\Database\QueryException $e) {
-                Session::flash('error', 'Upload kip_foto_sebelum failed');
-                return redirect()->back();
-            }
-        }
+        //         Artisan::call('cache:clear');
+        //     } catch (\Illuminate\Database\QueryException $e) {
+        //         Session::flash('error', 'Upload kip_foto_sebelum failed');
+        //         return redirect()->back();
+        //     }
+        // }
         
-        if ($request->hasFile('kip_foto_sesudah')) 
-        {
-            try
-            {
-                $kip_foto_sesudah = $kip_no.'-sesudah.'.$request->file('kip_foto_sesudah')->getClientOriginalExtension();
+        // if ($request->hasFile('kip_foto_sesudah')) 
+        // {
+        //     try
+        //     {
+        //         $kip_foto_sesudah = $kip_no.'-sesudah.'.$request->file('kip_foto_sesudah')->getClientOriginalExtension();
                     
-                // delete if exists
-                if (Storage::disk('public')->exists( 'kip/'.$kip_foto_sesudah )) Storage::delete('public/kip/'.$kip_foto_sesudah);
-                $request->file('kip_foto_sesudah')->storeAs('public/kip', $kip_foto_sesudah);
+        //         // delete if exists
+        //         if (Storage::disk('public')->exists( 'kip/'.$kip_foto_sesudah )) Storage::delete('public/kip/'.$kip_foto_sesudah);
+        //         $request->file('kip_foto_sesudah')->storeAs('public/kip', $kip_foto_sesudah);
 
-                Artisan::call('cache:clear');
-            } catch (\Illuminate\Database\QueryException $e) {
-                Session::flash('error', 'Upload kip_foto_sesudah failed');
-                return redirect()->back();
-            }
-        }
+        //         Artisan::call('cache:clear');
+        //     } catch (\Illuminate\Database\QueryException $e) {
+        //         Session::flash('error', 'Upload kip_foto_sesudah failed');
+        //         return redirect()->back();
+        //     }
+        // }
         
-        $kip = new Kip;
-        $kip->kip_no                = $kip_no;
-        $kip->kip_status            = $request->mode;
-        $kip->kip_judul_tema        = $request->kip_judul_tema;
-        $kip->kip_created_by        = Auth::user()->user_npk;
-        $kip->kip_created_on        = date("Y-m-d H:i:s");
-        $kip->kip_kategori          = $request->kip_kategori;
-        $kip->kip_line              = ($request->kip_line!="") ? $request->kip_line : null;
-        $kip->kip_proses            = ($request->kip_proses!="") ? $request->kip_proses : null;
-        $kip->kip_masalah           = htmlspecialchars($request->kip_masalah);
-        $kip->kip_perbaikan         = htmlspecialchars($request->kip_perbaikan);
-        $kip->kip_foto_sebelum      = ($request->hasFile('kip_foto_sebelum')) ? $kip_foto_sebelum : null;
-        $kip->kip_foto_sesudah      = ($request->hasFile('kip_foto_sesudah')) ? $kip_foto_sesudah : null;
-        $kip->kip_eval_uraian       = htmlspecialchars($request->kip_eval_uraian);
-        $kip->kip_eval_biaya        = htmlspecialchars($request->kip_eval_biaya);
-        $kip->kip_eval_benefit_kuantitatif  = htmlspecialchars($request->kip_eval_benefit_kuantitatif);
-        $kip->kip_eval_benefit_kualitatif   = htmlspecialchars($request->kip_eval_benefit_kualitatif);
-        $kip->kip_pengontrolan      = htmlspecialchars($request->kip_pengontrolan);
-        $simpankip = $kip->save();
+        // $kip = new Kip;
+        // $kip->kip_no                = $kip_no;
+        // $kip->kip_status            = $request->mode;
+        // $kip->kip_judul_tema        = $request->kip_judul_tema;
+        // $kip->kip_created_by        = Auth::user()->user_npk;
+        // $kip->kip_created_on        = date("Y-m-d H:i:s");
+        // $kip->kip_kategori          = $request->kip_kategori;
+        // $kip->kip_line              = ($request->kip_line!="") ? $request->kip_line : null;
+        // $kip->kip_proses            = ($request->kip_proses!="") ? $request->kip_proses : null;
+        // $kip->kip_masalah           = htmlspecialchars($request->kip_masalah);
+        // $kip->kip_perbaikan         = htmlspecialchars($request->kip_perbaikan);
+        // $kip->kip_foto_sebelum      = ($request->hasFile('kip_foto_sebelum')) ? $kip_foto_sebelum : null;
+        // $kip->kip_foto_sesudah      = ($request->hasFile('kip_foto_sesudah')) ? $kip_foto_sesudah : null;
+        // $kip->kip_eval_uraian       = htmlspecialchars($request->kip_eval_uraian);
+        // $kip->kip_eval_biaya        = htmlspecialchars($request->kip_eval_biaya);
+        // $kip->kip_eval_benefit_kuantitatif  = htmlspecialchars($request->kip_eval_benefit_kuantitatif);
+        // $kip->kip_eval_benefit_kualitatif   = htmlspecialchars($request->kip_eval_benefit_kualitatif);
+        // $kip->kip_pengontrolan      = htmlspecialchars($request->kip_pengontrolan);
+        // $simpankip = $kip->save();
 
         
-        if($simpankip)
-        {
-            if($request->mode == "draft")
-                Session::flash('success', 'KIP saved as draft');
-            elseif($request->mode == "draft")
-                Session::flash('success', 'KIP submitted succesfuly');
+        // if($simpankip)
+        // {
+        //     if($request->mode == "draft")
+        //         Session::flash('success', 'KIP saved as draft');
+        //     elseif($request->mode == "draft")
+        //         Session::flash('success', 'KIP submitted succesfuly');
 
-        } else {
-            Session::flash('error', 'Menyimpan kegiatan gagal! Mohon hubungi admin MagangHub');
-        }
+        // } else {
+        //     Session::flash('error', 'Menyimpan kegiatan gagal! Mohon hubungi admin MagangHub');
+        // }
         
-        return redirect()->back();
+        // return redirect()->back();
     }
 }
