00474841a5a4b8e910eff4eababc5bce5348d2a5
Tedo <tedohac@gmail.com>
2023-02-11 23:17:57 +0700

    20230211

--- resources/views/kip/list.blade.php
+++ resources/views/kip/list.blade.php
@@ -64,7 +64,7 @@
               <a class="btn btn-outline-info p-1 float-right broadcast-form" href="{{ route('kip.edit', $kip->kip_no) }}">
                 <i class="fa fa-fw fa-edit"></i>
               </a>
-              @elseif( $kip->kip_status=='submit' )
+              @else
               <a class="btn btn-outline-info p-1 float-right broadcast-form" href="{{ route('kip.view', $kip->kip_no) }}">
                 <i class="fa fa-fw fa-search"></i>
               </a>
