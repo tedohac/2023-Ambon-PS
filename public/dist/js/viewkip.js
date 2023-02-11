2e1512b26ab4d41a6db0c1763e46f6973cd6a8f0
164367 MUHAMAD <164367.MUHAMAD@klgroup.local>
2023-02-11 12:24:19 +0700

    20230210

--- public/dist/js/viewkip.js
+++ public/dist/js/viewkip.js
@@ -1,9 +1,6 @@
 
     $(document).ready(function(){
         // BS-Stepper Init
-        $(document).ready(function () {
-            var stepper = new Stepper($('.bs-stepper'))
-            
-            stepper.to(2)
-        })
+        var stepper = new Stepper($('.bs-stepper'))
+        stepper.to(2)
     });
