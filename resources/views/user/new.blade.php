8eed6a6e5a5039cbee81f37b9a8aeb15caad87ee
Tedo <tedohac@gmail.com>
2023-02-17 10:57:50 +0700

    20230217

--- resources/views/user/new.blade.php
+++ resources/views/user/new.blade.php
@@ -116,6 +116,27 @@
   <i class="fa fa-fw fa-save"></i>
   Save
 </button>
+
+<div class="modal fade" id="modal-submit">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Konfirmasi</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>User baru akan terbentuk</p>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary" value="submit">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 </form>
 @endsection
 
